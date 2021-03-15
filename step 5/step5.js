08b9f136123843b2b4329a9273aff2e9f8ad3d8f
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-03-15 13:07:23 +0200

    completed step-5

---
+++ step 5/step5.js
@@ -0,0 +1,11 @@
+var a = document.querySelector("#image1");
+var b = document.querySelector("#image2");
+var c = document.querySelector("#image3");
+var d = document.querySelector("#image4");
+var e = document.querySelector("#image5");
+var z;
+var x;
+var y ;
+var c ;
+var v ;
+
